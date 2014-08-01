a8200035254f0fbbe163a48af734f6c62704bc73
Gabor Remenyi <gabor.remenyi@fusepool.net>
2014-08-01 10:11:15 +0200

    Importing the pipeline-transformer project

---
+++ p3-pipeline-transformer/src/main/java/eu/fusepoolp3/p3pipelinetrasformer/App.java
@@ -0,0 +1,13 @@
+package eu.fusepoolp3.p3pipelinetrasformer;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
