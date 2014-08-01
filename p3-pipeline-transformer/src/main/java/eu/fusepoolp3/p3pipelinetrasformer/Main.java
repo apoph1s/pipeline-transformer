ce97839d5720f3e3e5884c873fb0861bd36ce2db
Gabor Remenyi <gabor.remenyi@fusepool.net>
2014-08-01 11:51:31 +0200

    submodule

--- p3-pipeline-transformer/src/main/java/eu/fusepoolp3/p3pipelinetrasformer/Main.java
+++ p3-pipeline-transformer/src/main/java/eu/fusepoolp3/p3pipelinetrasformer/Main.java
@@ -3,21 +3,20 @@ package eu.fusepoolp3.p3pipelinetrasformer;
 import eu.fusepool.extractor.server.ExtractorServer;
 import org.wymiwyg.commons.util.arguments.ArgumentHandler;
 
+public class Main {
 
-public class Main 
-{
     public static void main(String[] args) throws Exception {
         Arguments arguments = ArgumentHandler.readArguments(Arguments.class, args);
         if (arguments != null) {
             start(arguments);
         }
     }
-    
+
     private static void start(Arguments arguments) throws Exception {
         ExtractorServer server = new ExtractorServer(arguments.getPort());
 
         //DictionaryMatcher.getInstance();
-        
+
         server.start(new PipelineTransformer());
 
         server.join();
